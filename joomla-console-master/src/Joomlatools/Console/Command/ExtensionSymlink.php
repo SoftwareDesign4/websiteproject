d4f85f2abe939aefcfb6568dabcf302b9fd55cdb
Aba Radebe <aba.radebe@gmail.com>
2014-10-31 18:54:57 +0200

    Joomla Command Line Tools
    This is a script developed by Joomlatools to ease the management of
    Joomla sites.
    
    It is designed to work on Linux and MacOS. Windows users can use it in
    Joomlatools Vagrant box

---
+++ joomla-console-master/src/Joomlatools/Console/Command/ExtensionSymlink.php
@@ -0,0 +1,109 @@
+<?php
+/**
+ * @copyright	Copyright (C) 2007 - 2014 Johan Janssens and Timble CVBA. (http://www.timble.net)
+ * @license		Mozilla Public License, version 2.0
+ * @link		http://github.com/joomlatools/joomla-console for the canonical source repository
+ */
+
+namespace Joomlatools\Console\Command;
+
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ExtensionSymlink extends SiteAbstract
+{
+    protected $symlink = array();
+
+    protected function configure()
+    {
+        parent::configure();
+
+        $this
+            ->setName('extension:symlink')
+            ->setDescription('Symlink projects into a site')
+            ->addArgument(
+                'symlink',
+                InputArgument::REQUIRED | InputArgument::IS_ARRAY,
+                'A list of folders to symlink from projects folder'
+            )
+            ->addOption(
+                'projects-dir',
+                null,
+                InputOption::VALUE_REQUIRED,
+                'Directory where your custom projects reside',
+                sprintf('%s/Projects', trim(`echo ~`))
+            );
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        parent::execute($input, $output);
+
+        $this->symlink = $input->getArgument('symlink');
+
+        $this->check($input, $output);
+        $this->symlinkProjects($input, $output);
+    }
+
+    public function check(InputInterface $input, OutputInterface $output)
+    {
+        if (!file_exists($this->target_dir)) {
+            throw new \RuntimeException(sprintf('Site not found: %s', $this->site));
+        }
+    }
+
+    public function symlinkProjects(InputInterface $input, OutputInterface $output)
+    {
+        static $dependencies = array(
+            'extman'  => array('koowa'),
+            'docman'  => array('extman', 'koowa', 'com_files'),
+            'fileman' => array('extman', 'koowa', 'com_files'),
+            'logman'  => array('extman', 'koowa', 'com_activities')
+        );
+
+        $project_folder = $input->getOption('projects-dir');
+        $destination    = $this->target_dir;
+
+        $projects = array();
+        foreach ($this->symlink as $symlink)
+        {
+            $projects[] = $symlink;
+            if (array_key_exists($symlink, $dependencies)) {
+                $projects = array_merge($projects, $dependencies[$symlink]);
+            }
+        }
+
+        // If we are symlinking Koowa, we need to create this structure to allow multiple symlinks in them
+        if (in_array('koowa', $projects))
+        {
+            $dirs = array($this->target_dir.'/libraries/koowa/components', $this->target_dir.'/media/koowa');
+            foreach ($dirs as $dir)
+            {
+                if (!is_dir($dir)) {
+                    mkdir($dir, 0777, true);
+                }
+            }
+        }
+
+        foreach ($projects as $project)
+        {
+            $root = $project_folder.'/'.$project;
+
+            if (!is_dir($root)) {
+                continue;
+            }
+
+            if (is_dir($root.'/code')) {
+                $root = $root.'/code';
+            }
+
+            $iterator = new Symlink\Iterator($root, $destination);
+
+            while ($iterator->valid()) {
+                $iterator->next();
+            }
+        }
+    }
+}
---
+++ joomla-console-master/src/Joomlatools/Console/Command/SiteDelete.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * @copyright	Copyright (C) 2007 - 2014 Johan Janssens and Timble CVBA. (http://www.timble.net)
+ * @license		Mozilla Public License, version 2.0
+ * @link		http://github.com/joomlatools/joomla-console for the canonical source repository
+ */
+
+namespace Joomlatools\Console\Command;
+
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class SiteDelete extends SiteAbstract
+{
+    protected function configure()
+    {
+        parent::configure();
+
+        $this
+            ->setName('site:delete')
+            ->setDescription('Delete a site');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        parent::execute($input, $output);
+
+        $this->deleteFolder($input, $output);
+        $this->deleteVirtualHost($input, $output);
+        $this->deleteDatabase($input, $output);
+    }
+
+    public function deleteFolder(InputInterface $input, OutputInterface $output)
+    {
+        `rm -rf $this->target_dir`;
+    }
+
+    public function deleteDatabase(InputInterface $input, OutputInterface $output)
+    {
+        $password = empty($this->mysql->password) ? '' : sprintf("-p'%s'", $this->mysql->password);
+        $command  = sprintf("echo 'DROP DATABASE IF EXISTS `$this->target_db`' | mysql -u'%s' %s", $this->mysql->user, $password);
+
+        $result   = exec($command);
+
+        if (!empty($result)) { // MySQL returned an error
+            throw new \RuntimeException(sprintf('Cannot delete database %s. Error: %s', $this->target_db, $result));
+        }
+    }
+
+    public function deleteVirtualHost(InputInterface $input, OutputInterface $output)
+    {
+        if (is_file('/etc/apache2/sites-available/1-'.$this->site.'.conf'))
+        {
+            `sudo a2dissite 1-$this->site.conf`;
+            `sudo rm -f /etc/apache2/sites-available/1-$this->site.conf`;
+            `sudo /etc/init.d/apache2 restart > /dev/null 2>&1`;
+        }
+    }
+}
